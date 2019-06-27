2a5fb29f5ef4103bcf0311dfa76944efc8814a45
Lina Maria Villa <50224761+linavilla@users.noreply.github.com>
2019-06-26 22:39:18 -0500

    Update q10.py

--- q10.py
+++ q10.py
@@ -4,3 +4,12 @@
 ## de la tabla tbl1.tsv
 ## 
 
+import pandas as pd
+df1 = pd.read_csv('tbl1.tsv', sep='\t')
+df1.head()
+dfaux = df1.groupby('_c0')['_c4'].apply(list)
+df = pd.DataFrame()
+df['_c0'] = dfaux.keys()
+df['lista'] = [elem for elem in dfaux]
+df['lista'] = [",".join(str(v) for v in sorted(elem)) for elem in df['lista']]
+df

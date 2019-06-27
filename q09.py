373a489b45dd8258637a19f0c7d2567f48c6eb19
Lina Maria Villa <50224761+linavilla@users.noreply.github.com>
2019-06-26 22:39:13 -0500

    Update q09.py

--- q09.py
+++ q09.py
@@ -3,5 +3,11 @@
 ## separada por ':' de los valores de la columna _c2
 ## para el archivo tbl0.tsv
 ## 
-
-
+import pandas as pd
+df0 = pd.read_csv('tbl0.tsv', sep='\t')
+dfaux = df0.groupby('_c1')['_c2'].apply(list)
+df = pd.DataFrame()
+df['_c1'] = dfaux.keys()
+df['_c2'] = [elem for elem in dfaux]
+df['_c2'] = [":".join(str(v) for v in sorted(elem)) for elem in df['_c2']]
+df

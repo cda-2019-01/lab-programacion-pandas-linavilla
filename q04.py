9cf6dceaa7a6b17c05b66568b41a49af88a6b998
Lina Maria Villa <50224761+linavilla@users.noreply.github.com>
2019-06-26 22:38:33 -0500

    Update q04.py

--- q04.py
+++ q04.py
@@ -3,4 +3,11 @@
 ## de la tabla tbl1 en mayusculas
 ## 
 
-
+import pandas as pd
+df1 = pd.read_csv('tbl1.tsv', sep='\t')
+uniques = df1['_c4'].unique()
+res = []
+for element in uniques:
+    res.append(element.upper())
+res = sorted(res)
+res
